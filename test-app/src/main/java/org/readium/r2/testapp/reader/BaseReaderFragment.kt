983bbb8ea0d9ac0a7bc1bccaf13abfac6dca64ac
Mathieu Jehanno <mathieujehannodu26@gmail.com>
2024-03-27 13:19:25 +0100

    Ajout bouton pour générer un nouveau fichier divina en personnalisant le type de police du texte dans les bulles des BD au format divina, la couleur du texte et la couleur de fond de la bulle.
    Ajout bouton pour générer un nouveau fichier divina en modifiant le fichier d'origine pour permettre un accessibilité plus avancée.
    Choix entre trois modes d'accessibilité:
    - Highlight : mise en surbrillance des éléments de type case, panneaux bulle dans la BD
    - ZoomIn : agrandissement des cases par dessus la page d'origine
    - ZoomInBlack : agrandissement des cases par dessus un fond noir
    
    Mise en place de l'environnement chaquopy dans le build.gradle.kts pour permettre d'exécuter du code python (correspondant aux programmes appelés par les boutons).

--- test-app/src/main/java/org/readium/r2/testapp/reader/BaseReaderFragment.kt
+++ test-app/src/main/java/org/readium/r2/testapp/reader/BaseReaderFragment.kt
@@ -132,40 +132,49 @@ abstract class BaseReaderFragment : Fragment() {
         navigator.go(locator, animated)
     }
 
+    // Méthode pour afficher la boîte de dialogue des options de personnalisation de la police
     private fun showfontpersonnalisationOptionsDialog(context: Context) {
+        // Listes des noms de police et de leurs identifiants
         val fontNamesChoice = arrayOf("Arial", "Helvetica", "Calibri", "Verdana", "Tahoma", "Open Sans", "Luciole")
         val fontNamesId = arrayOf("arial.ttf", "helvetica.ttf", "calibri.ttf", "verdana.ttf", "tahoma.ttf", "opensans.ttf", "Luciole-Regular.ttf")
 
+        // Listes des couleurs de police et de leurs identifiants
         val fontColorsChoice = arrayOf("Blue", "Yellow", "Red", "Green", "Purple", "Orange", "Pink", "Turquoise", "Brown", "Cyan", "Magenta", "Gold", "Silver", "Maroon", "Navy", "Black", "White")
         val fontColorsId = arrayOf("blue", "yellow", "red", "green", "purple", "orange", "pink", "turquoise", "brown", "cyan", "magenta", "gold", "silver", "maroon", "navy", "black", "white")
 
+        // Listes des couleurs de fond et de leurs identifiants
         val backgroundColorsChoice = arrayOf("Blue", "Yellow", "Red", "Green", "Purple", "Orange", "Pink", "Turquoise", "Brown", "Cyan", "Magenta", "Gold", "Silver", "Maroon", "Navy", "Black", "White")
         val backgroundColorsId = arrayOf("blue", "yellow", "red", "green", "purple", "orange", "pink", "turquoise", "brown", "cyan", "magenta", "gold", "silver", "maroon", "navy", "black", "white")
 
+        // Création du constructeur de la boîte de dialogue
         val builder = AlertDialog.Builder(context)
         builder.setTitle("Personnaliser le Texte des Bulles")
 
+        // Catégories des options de personnalisation
         val categories = arrayOf("Type de police", "Couleur de la police", "Couleur de fond", "Sélectionner le fichier")
         val options = arrayOf(fontNamesChoice, fontColorsChoice, backgroundColorsChoice, arrayOf("Sélectionner le fichier DiViNa"))
 
+        // Affichage des catégories dans la boîte de dialogue
         builder.setItems(categories) { _, categoryIndex ->
             val categoryOptions = options[categoryIndex]
 
+            // Constructeur pour la sélection des options de chaque catégorie
             val categoryBuilder = AlertDialog.Builder(context)
             categoryBuilder.setTitle(categories[categoryIndex])
             categoryBuilder.setItems(categoryOptions) { _, which ->
-                // Assign the selected option based on the category
+
+                // Attribution de l'option sélectionnée en fonction de la catégorie
                 when (categoryIndex) {
-                    0 -> selectedFontIdToLaunch = fontNamesId[which]
+                    0 -> selectedFontIdToLaunch = fontNamesChoice[which]
                     1 -> selectedFontColorIdToLaunch = fontColorsId[which]
                     2 -> selectedBackgroundColorIdToLaunch = backgroundColorsId[which]
                     3 -> {
-                        // Open the file picker for selecting file
+                        // Ouvrir le sélecteur de fichier pour sélectionner un fichier
                         openFileLauncherFont.launch("application/*")
-                        return@setItems  // Return immediately after launching file picker
+                        return@setItems  // Retourner immédiatement après l'ouverture du sélecteur de fichier
                     }
                 }
-                // Show the category selection dialog again
+                // Afficher à nouveau la boîte de dialogue de sélection de catégorie
                 showfontpersonnalisationOptionsDialog(context)
             }
             categoryBuilder.setNegativeButton("Cancel", null)
@@ -178,15 +187,15 @@ abstract class BaseReaderFragment : Fragment() {
         dialog.show()
     }
 
-    // Déclarez un launcher pour l'ouverture du sélecteur de fichiers
+    // Déclaration d'un launcher pour l'ouverture du sélecteur de fichiers
     private val openFileLauncherFont = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         uri?.let { fileUri ->
-            // Obtenez le chemin du fichier à partir de l'URI
+            // Obtention du chemin du fichier à partir de l'URI
             val filePath = getPathFromUri(requireContext(), fileUri)
 
-            // Vérifiez si le fichier a l'extension .divina
+            // Vérification si le fichier a l'extension .divina
             if (filePath != null && filePath.endsWith(".divina")) {
-                // Lancez le programme Python avec le mode d'accessibilité et le chemin du fichier Divina
+                // Lancement du programme Python avec les informations de personnalisation des bulles et le chemin du fichier Divina
                 filePathToWriteFont = filePath
 
                 Log.d("Parameter_Debug", "selectedFontIdToLaunch: $selectedFontIdToLaunch")
@@ -200,15 +209,16 @@ abstract class BaseReaderFragment : Fragment() {
         }
     }
 
+    // Méthode pour lancer le programme Python avec les paramètres de personnalisation de la police
     private fun launchPythonProgramFont(selectedFontId: String?, filePath: String, fontColorId: String?, backgroundColorId: String?) {
 
-        // Check if permission is already granted
+        // Vérifier si la permission est déjà accordée
         if (ContextCompat.checkSelfPermission(
                 requireActivity(),
                 Manifest.permission.MANAGE_EXTERNAL_STORAGE
             ) == PackageManager.PERMISSION_GRANTED
         ) {
-            // Permission already granted, launch the Python program
+            // La permission est déjà accordée, lancer le programme Python
             if (!Python.isStarted()) {
                 Python.start(AndroidPlatform(requireActivity()))
             }
@@ -217,19 +227,20 @@ abstract class BaseReaderFragment : Fragment() {
             val result = mainPersonnalisationModule.callAttr(
                 "main",
                 filePath,
-                selectedFontId ?: "Arial", // Default font if not provided
-                fontColorId ?: "black", // Default font color if not provided
-                backgroundColorId ?: "white" // Default background color if not provided
+                selectedFontId ?: "Arial", // Police par défaut si non fournie
+                fontColorId ?: "black", // Couleur de police par défaut si non fournie
+                backgroundColorId ?: "white" // Couleur de fond par défaut si non fournie
             ).toString()
             Log.d("Parameter_Debug", "les paramètres: $result")
             Toast.makeText(requireActivity(), result, Toast.LENGTH_SHORT).show()
 
         } else {
-            // Permission not granted, request the permission
+            // La permission n'est pas accordée, demander la permission
             requestAccessStoragePermissionLauncherfont.launch(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
         }
     }
 
+    // Launcher pour demander l'autorisation d'accès au stockage pour l'exécution du programme Python
     private val requestAccessStoragePermissionLauncherfont = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
@@ -247,12 +258,18 @@ abstract class BaseReaderFragment : Fragment() {
             Toast.makeText(requireActivity(), "Permission refusée pour écrire dans le stockage externe", Toast.LENGTH_SHORT).show()
         }
     }
+
+    // Méthode pour afficher la boîte de dialogue des options d'accessibilité
     private fun showAccessibilityOptionsDialog(context: Context) {
+        // Options de modes d'accessibilité et leurs identifiants
         val accessibilityModes = arrayOf("Surbrillance", "Agrandissement", "Agrandissement isolé")
         val accessibilityModesIds = arrayOf("Highlight", "ZoomIn", "ZoomInBlack")
 
+        // Création du constructeur de la boîte de dialogue
         val builder = AlertDialog.Builder(context)
         builder.setTitle("Choisissez un mode d'accessibilité")
+
+        // Affichage des options de modes d'accessibilité dans la boîte de dialogue
         builder.setItems(accessibilityModes) { _, which ->
             val selectedModeId = accessibilityModesIds[which]
             selectedAccessibilityMode = selectedModeId // Stockez le mode sélectionné
@@ -265,15 +282,15 @@ abstract class BaseReaderFragment : Fragment() {
         dialog.show()
     }
 
-    // Déclarez un launcher pour l'ouverture du sélecteur de fichiers
+    // Déclaration un launcher pour l'ouverture du sélecteur de fichiers
     private val openFileLauncherAccessibility = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
         uri?.let { fileUri ->
-            // Obtenez le chemin du fichier à partir de l'URI
+            // Obtention du chemin du fichier à partir de l'URI
             val filePath = getPathFromUri(requireContext(), fileUri)
 
-            // Vérifiez si le fichier a l'extension .divina
+            // Vérification si le fichier a l'extension .divina
             if (filePath != null && filePath.endsWith(".divina")) {
-                // Lancez le programme Python avec le mode d'accessibilité et le chemin du fichier Divina
+                // Lancement du programme Python avec le mode d'accessibilité et le chemin du fichier Divina
                 filePathToWriteAccessibility = filePath
 
                 Log.d("Parameter_Debug", "Path du fichier divina: $filePathToWriteAccessibility")
@@ -286,46 +303,57 @@ abstract class BaseReaderFragment : Fragment() {
         }
     }
 
+    // Méthode pour obtenir le chemin du fichier à partir de son URI
     private fun getPathFromUri(context: Context, uri: Uri): String? {
+        // Vérifier si l'URI correspond à un document
         if (DocumentsContract.isDocumentUri(context, uri)) {
-            val documentId = DocumentsContract.getDocumentId(uri)
-            val split = documentId.split(":")
+            val documentId = DocumentsContract.getDocumentId(uri) // Obtenir l'identifiant du document
+            val split = documentId.split(":") // Séparer l'identifiant du type de stockage
+
+            // Vérifier si l'identifiant est au bon format
             if (split.size == 2) {
-                val storageType = split[0]
+                val storageType = split[0] // Récupérer le type de stockage
+
+                // Si le type de stockage est "raw", le chemin est directement spécifié
                 if ("raw" == storageType) {
-                    return split[1]
+                    return split[1] // Retourner le chemin du fichier
                 } else {
-                    val contentUri = when (storageType) {
+                    val contentUri = when (storageType) { // Déterminer le type de contenu de l'URI
+                        // Si le type de stockage est externe
                         "com.android.externalstorage.documents" -> {
-                            val volumeId = split[0]
-                            val path = split[1]
-                            val rootUri = MediaStore.Files.getContentUri(volumeId)
-                            ContentUris.withAppendedId(rootUri, path.toLong())
+                            val volumeId = split[0] // Récupérer l'identifiant du volume
+                            val path = split[1] // Récupérer le chemin
+                            val rootUri = MediaStore.Files.getContentUri(volumeId) // Obtenir l'URI de base du stockage externe
+                            ContentUris.withAppendedId(rootUri, path.toLong()) // Créer l'URI du fichier
                         }
+                        // Si le type de stockage est les téléchargements
                         "com.android.providers.downloads.documents" -> {
+                            // Obtenir l'URI du fichier à partir de l'identifiant du document
                             ContentUris.withAppendedId(
                                 Uri.parse("content://downloads/public_downloads"),
                                 split[1].toLong()
                             )
                         }
+                        // Si le type de stockage n'est pas pris en charge, utiliser directement l'URI fournie
                         else -> uri
                     }
+                    // Effectuer une requête pour obtenir le chemin réel du fichier à partir de l'URI
                     context.contentResolver.query(
                         contentUri,
-                        arrayOf(MediaStore.Files.FileColumns.DATA),
+                        arrayOf(MediaStore.Files.FileColumns.DATA), // Demander la colonne de données (chemin du fichier)
                         null,
                         null,
                         null
                     )?.use { cursor ->
-                        val columnIndex = cursor.getColumnIndex(MediaStore.Files.FileColumns.DATA)
-                        val path = if (columnIndex != -1) {
-                            cursor.getString(columnIndex)
+                        val columnIndex = cursor.getColumnIndex(MediaStore.Files.FileColumns.DATA) // Obtenir l'indice de la colonne de données
+                        val path = if (columnIndex != -1) { // Vérifier si la colonne existe dans le curseur
+                            cursor.getString(columnIndex) // Récupérer le chemin du fichier à partir du curseur
                         } else {
                             // La colonne n'existe pas dans le curseur
                             null
                         }
                         cursor.close()
-                        return path
+                        return path // Retourner le chemin du fichier
                     }
                 }
             }
@@ -333,34 +361,39 @@ abstract class BaseReaderFragment : Fragment() {
         return null
     }
 
+    // Launcher pour demander la permission d'accès au stockage pour l'exécution du programme Python
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
+
         if (isGranted) {
-            // Permission granted, execute the Python code
+            // Si la permission est accordée, Exécuter le code Python
             if (!Python.isStarted()) {
                 Python.start(AndroidPlatform(requireActivity()))
             }
             val python = Python.getInstance()
             val mainAccessibilityModule = python.getModule("main_accessibility")
             val result = mainAccessibilityModule.callAttr("main", filePathToWriteAccessibility, selectedAccessibilityMode).toString()
-            Toast.makeText(requireActivity(), result, Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireActivity(), result, Toast.LENGTH_SHORT).show() // Afficher le résultat dans un toast
         } else {
-            // Permission denied, show a toast
+            // Si la permission est refusée, Afficher un toast informant que la permission est refusée
             Toast.makeText(requireActivity(), "Permission refusée pour lire et écrire dans le stockage externe.", Toast.LENGTH_SHORT).show()
         }
     }
 
-    // Méthode pour lancer le programme Python
+    // Méthode pour lancer le programme Python avec les paramètres d'accessibilité sélectionnés
     private fun launchPythonProgramAccessibility(selectedModeId: String?, filePath: String) {
         if (shouldRequestPermission()) {
+            // Vérifier si la permission doit être demandée
             if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.MANAGE_EXTERNAL_STORAGE)) {
+                // Afficher un message si l'autorisation est nécessaire
                 Toast.makeText(requireActivity(), "La permission d'accès au stockage est nécessaire pour exécuter le programme Python.", Toast.LENGTH_LONG).show()
             } else {
+                // Demander la permission d'accès au stockage
                 requestPermissionLauncher.launch(Manifest.permission.MANAGE_EXTERNAL_STORAGE)
             }
         } else {
-            // Permission already granted, execute the Python code
+            // La permission est déjà accordée, exécuter le code Python
             if (!Python.isStarted()) {
                 Python.start(AndroidPlatform(requireActivity()))
             }
@@ -371,6 +404,7 @@ abstract class BaseReaderFragment : Fragment() {
         }
     }
 
+    // Méthode pour vérifier si la permission doit être demandée
     private fun shouldRequestPermission(): Boolean {
         return ContextCompat.checkSelfPermission(
             requireActivity(),
@@ -378,6 +412,7 @@ abstract class BaseReaderFragment : Fragment() {
         ) != PackageManager.PERMISSION_GRANTED
     }
 
+    // Méthode pour afficher les erreurs
     protected fun showError(error: UserException) {
         val context = context ?: return
         Toast.makeText(context, error.getUserMessage(context), Toast.LENGTH_LONG).show()
